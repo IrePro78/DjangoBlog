b3d0cdc333bba9b7a1bfb9e0ea193798f51dbdc1
IrePro78 <gos3r78@gmail.com>
2021-11-03 13:40:47 +0100

    add_class_to_view, add_mixin

--- APIBlog/views.py
+++ APIBlog/views.py
@@ -1,11 +1,48 @@
 from .serializers import ArticleSerializer
 from rest_framework.decorators import api_view, APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics, mixins
 from .models import Article
 
 
 
+class ArticleList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+class ArticleDetails(generics.GenericAPIView,
+                    mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,):
+
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+    lookup_field = 'id'
+
+
+    def get(self, request, id):
+        return self.retrieve(request, id=id)
+
+    def put(self, request, id):
+        return self.update(request, id=id)
+
+    def delete(self, request, id):
+        return self.destroy(request, id=id)
+
+
+
+
+
+
+"""
 class ArticleList(APIView):
 
     def get(self, request):
@@ -48,3 +85,4 @@ class ArticleDetails(APIView):
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+"""
